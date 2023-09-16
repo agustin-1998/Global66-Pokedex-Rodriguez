4bfe0e69799431dee43278d77609a8d5ae9f19a3
Agus <agus.rodriguez777@gmail.com>
2023-09-16 00:39:06 -0300

    add

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="fixed bottom-0 bg-white w-full z-20">
         <div class="max-w-xl grid grid-cols-2 gap-x-5 py-4 px-7 sm:px-0 mx-auto">
-            <router-link class="mx-auto px-5 py-3 bg-gray-bf rounded-full text-white-f5 transition-colors 0.1s w-full text-center flex justify-center items-center gap-x-3 hover:bg-gray-5e" :to="{ name: 'list' }" @click="resetQuery">
+            <router-link class="mx-auto px-5 py-3 rounded-full bg-gray-bf transition-colors 0.1s w-full text-center text-white flex justify-center items-center gap-x-3 hover:bg-gray-5e" :to="{ name: 'list' }" @click="resetQuery">
                 <figure>
                     <img src="../assets/img/allIcon.svg" alt="all-icon">
                 </figure>
