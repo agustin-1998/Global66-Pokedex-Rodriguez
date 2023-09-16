945d6b929b1d7c020ca17c0713ef50eeccd281f9
Agus <agus.rodriguez777@gmail.com>
2023-09-16 00:51:20 -0300

    add

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,17 @@
 <template>
-    <main class="bg-white-f9 min-h-screen">
+    <main class="bg-white-f9 min-h-screen main-layout">
         <slot></slot>
     </main>
 </template>
+
+<style>
+    .main-layout::-webkit-scrollbar  {
+        width: 0;
+    }
+
+    .main-layout {
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+    }
+</style>
+
